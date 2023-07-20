92b74253140ad46b993f41f0e26ae8bea82b7509
rhonvyramos <rhonvyramos@gmail.com>
2023-07-20 11:59:41 -0400

    fixed duplicate inquirer prompt

--- index.js
+++ index.js
@@ -18,10 +18,6 @@ const questions = [
     },
     {
         name: "usage",
-        message: "Describe your project: ->",
-    },
-    {
-        name: "description",
         message: "Detail how your project is used: ->",
     },
     {
@@ -105,37 +101,24 @@ function write_readme(answers) {
     // destructuring answers object into individual variables
     const {title, description, installation, usage, credits, features, contribution, tests, license, github, email} = answers;
 
-    // assigns string template using answers variables
-    let readMe_text = 
-    `# ${title}\n` + 
-    `## Table of Contents\n- **[Description](#description)**\n\n- **[Installation](#installation)**` + 
-    `\n\n- **[Usage](#usage)**\n\n- **[Credits](#credits)**\n\n- **[Features](#features)**\n\n- **[Contribution](#contribute)**\n\n- **[Tests](#tests)**\n` +
-    `## Description\n${description}\n## Installation\n${installation}\n## Usage\n${usage}\n## Credits\n${credits}\n` +
-    `## License\nThis project is licensed under the terms of the MIT license.\n` + 
-    `\n![Static Badge](https://img.shields.io/badge/build-MIT-brightgreen?label=License)` +
-    `\n## Features\n${features}\n## Contribution\n${contribution}\n## Tests\n${tests}`
-
     // assigns tring template using answers variables to create the full readme
     let readMe_text_complete = 
-    // license badge
-    `![License Badge](https://img.shields.io/badge/License-${license}-brightgreen)\n` +
+
 
     // title
-    `# ${title}\n` +
+    `# ${title}\n
 
-    // table of contents
-    `## Table of Contents\n- **[Description](#description)**\n\n- **[Installation](#installation)**` +
-    `\n\n- **[Usage](#usage)**\n\n- **[Credits](#credits)**\n\n- **[Features](#features)**\n\n- ` +
-    `**[Contribution](#contribute)**\n\n- **[Tests](#tests)**\n\n- **[License](#license)**\n\n- **[Questions](#questions)**\n` +
+<p align="center"><img src="https://img.shields.io/badge/License-MIT_License-brightgreen!"></p> 
 
-    // description, installation, usage, features, contribution, and credits sections
-    `## Description\n${description}\n## Installation\n${installation}\n## Usage\n${usage}\n## Features\n${features}\n## Contribution\n${contribution}\n## Credits\n${credits}\n` +
+## Table of Contents\n- **[Description](#description)**\n\n- **[Installation](#installation)** 
+\n\n- **[Usage](#usage)**\n\n- **[Credits](#credits)**\n\n- **[Features](#features)**\n\n-  
+**[Contribution](#contribute)**\n\n- **[Tests](#tests)**\n\n- **[License](#license)**\n\n- **[Questions](#questions)**\n 
 
-    // license section
-    `## License\nThis project falls under the ${license}.\n` +
+## Description\n${description}\n## Installation\n${installation}\n## Usage\n${usage}\n## Features\n${features}\n## Contribution\n${contribution}\n## Credits\n${credits}\n 
 
-    // questions section
-    `## Questions\nThis project is available in the author's [GitHub](github.com/${github}). If any further questions are required, simply write an email to ${email}` 
+## License\nThis project falls under the ${license}.\n 
+
+## Questions\nThis project is available in the author's [GitHub](github.com/${github}). If any further questions are required, simply write an email to ${email}` 
 
     // returns new variable with full readMe string
     return readMe_text_complete;
--- written_files/README.md
+++ written_files/README.md
@@ -1,9 +1,13 @@
-![License Badge](https://img.shields.io/badge/License-MIT_License-brightgreen)
-# 
+# Title
+
+
+<p align="center"><img src="https://img.shields.io/badge/License-MIT_License-brightgreen!"></p> 
+
 ## Table of Contents
 - **[Description](#description)**
 
-- **[Installation](#installation)**
+- **[Installation](#installation)** 
+
 
 - **[Usage](#usage)**
 
@@ -11,26 +15,33 @@
 
 - **[Features](#features)**
 
-- **[Contribution](#contribute)**
+-  
+**[Contribution](#contribute)**
 
 - **[Tests](#tests)**
 
 - **[License](#license)**
 
 - **[Questions](#questions)**
+ 
+
 ## Description
-
+Desc
 ## Installation
-
+Install
 ## Usage
-
+Usage
 ## Features
-
+Features
 ## Contribution
-
+Contributions
 ## Credits
+Credits
+ 
 
 ## License
 This project falls under the MIT_License.
+ 
+
 ## Questions
-This project is available in the author's [GitHub](github.com/). If any further questions are required, simply write an email to 
+This project is available in the author's [GitHub](github.com/github). If any further questions are required, simply write an email to git@github.com
